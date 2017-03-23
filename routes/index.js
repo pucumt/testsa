dc19ecea23f41378136d8006ba57afd54c69fbd8
pucumt <weple@qq.com>
2017-03-23 17:33:27 +0800

    Add pressure test

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,8 @@ var client = require('./Client/index.js'),
     exec = require('child_process').exec, //,
     server = require('./Server/index.js'),
     test = require('./Test/index.js'),
-    generator = require('./Test/generator.js');
+    generator = require('./Test/generator.js'),
+    dbPressure = require('./Test/dbPressure.js');
 
 module.exports = function(app) {
     client(app);
@@ -11,6 +12,7 @@ module.exports = function(app) {
 
     test(app);
     generator(app);
+    dbPressure(app);
 
     app.use(function(req, res) {
         res.render("404");
