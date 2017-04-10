127c6740ac11a177892e7b49226215fc90f1775a
pucumt <weple@qq.com>
2017-04-10 21:26:36 +0800

    Implement reg function and delete user function is not correct.

--- routes/Client/index.js
+++ routes/Client/index.js
@@ -3,22 +3,15 @@ var enroll = require('./enroll.js'),
     login = require('./login.js'),
     user = require('./user.js'),
     order = require('./order.js'),
-    person = require('./person.js');
-// reg = require('./reg.js'),
+    person = require('./person.js'),
+    reg = require('./reg.js');
 
 module.exports = function(app) {
     home(app);
     enroll(app);
     login(app);
+    reg(app);
     user(app);
     person(app);
     order(app);
-
-    // logout(app);
-
-    // post(app);
-
-    // reg(app);
-
-    // eng100(app);
 };
--- routes/Client/reg.js
+++ routes/Client/reg.js
@@ -1,47 +1,53 @@
 var crypto = require('crypto'),
-	User = require('../../models/user.js'),
-	auth = require("./auth"),
-	checkNotLogin = auth.checkNotLogin;
+    StudentAccount = require('../../models/studentAccount.js'),
+    auth = require("./auth"),
+    checkNotLogin = auth.checkNotLogin;
 
 module.exports = function(app) {
-	app.get('/reg', checkNotLogin);
-	app.get('/reg', function(req, res) {
-		res.render('Client/reg', {
-			title: '注册',
-			user: req.session.user
-		});
-	});
+    app.get('/reg', checkNotLogin);
+    app.get('/reg', function(req, res) {
+        res.render('Client/reg.html', {
+            title: '注册',
+            user: req.session.user
+        });
+    });
 
-	app.post('/reg', checkNotLogin);
-	app.post('/reg', function(req, res) {
-		var name = req.body.name,
-			password = req.body.password,
-			password_re = req.body['password-repeat'];
-		//检验用户两次输入的密码是否一致
-		if (password_re != password) {
-			return res.redirect('/reg'); //返回主册页
-		}
-		//生成密码的 md5 值
-		var md5 = crypto.createHash('md5'),
-			password = md5.update(req.body.password).digest('hex');
-		var newUser = new User({
-			name: req.body.name,
-			password: password,
-			email: req.body.email
-		});
-		//检查用户名是否已经存在 
-		User.get(newUser.name, function(err, user) {
-			if (user) {
-				return res.redirect('/reg'); //返回注册页
-			}
-			//如果不存在则新增用户
-			newUser.save(function(err, user) {
-				if (err) {
-					return res.redirect('/reg'); //注册失败返回主册页
-				}
-				req.session.user = user; //用户信息存入 session
-				res.redirect('/'); //注册成功后返回主页
-			});
-		});
-	});
+    app.post('/reg', checkNotLogin);
+    app.post('/reg', function(req, res) {
+        var name = req.body.name,
+            password = req.body.password; //,
+        // password_re = req.body['password-repeat'];
+        //检验用户两次输入的密码是否一致
+        // if (password_re != password) {
+        // 	return res.redirect('/reg'); //返回主册页
+        // }
+        //生成密码的 md5 值
+        var md5 = crypto.createHash('md5'),
+            password = md5.update(req.body.password).digest('hex');
+        var newUser = new StudentAccount({
+            name: req.body.name,
+            password: password
+        });
+        //检查用户名是否已经存在 
+        StudentAccount.getFilter({ name: req.body.name })
+            .then(function(account) {
+                if (account) {
+                    return res.render('Client/reg.html', {
+                        title: '注册',
+                        error: "用户名已经存在"
+                    }); //返回注册页
+                } else {
+                    //如果不存在则新增用户
+                    newUser.save().then(function(user) {
+                        if (user) {
+                            return res.redirect('/login'); //注册成功
+                        }
+                        return res.render('Client/reg.html', {
+                            title: '注册',
+                            error: "注册失败"
+                        }); //返回注册页
+                    });
+                }
+            });
+    });
 }
