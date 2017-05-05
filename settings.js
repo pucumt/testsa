3a41bd567740fac2570e6630a492cb5ccbcc0b71
pucumt <weple@qq.com>
2017-05-05 10:39:35 +0800

    Add appid in settings

--- settings.js
+++ settings.js
@@ -1,12 +1,12 @@
 module.exports = {
     cookieSecret: 'BFBWX',
-    db: 'website',
+    db: 'websiteOnline',
     host: '127.0.0.1',
     port: '10105',
     key: '9d101c97133837e13dde2d32a5054abb', //e6371d360d79eb9fa4c25c7f91d2bc6b
     mch_id: '7551000001', //101560037142
     create_ip: '127.0.0.1',
     notify_Url: 'http://www.dushidao.com/admin/pay/notify',
-    AppID: 'wx3fc2b5ecc62fb706',
-    AppSecret: '9aa16bfd1a73d231130e51758036c23c'
+    AppID: 'wx3fc2b5ecc62fb706', //wxca435d4bf588644b
+    AppSecret: '9aa16bfd1a73d231130e51758036c23c' //c0e00d6f36f35819218032e18fa87a01
 };
