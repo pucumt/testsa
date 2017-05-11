cb1721056f93bb532b8b02acc1a5bcf4fe566330
pucumt <weple@qq.com>
2017-05-11 11:11:11 +0800

    new shop

--- settings.js
+++ settings.js
@@ -1,12 +1,12 @@
 module.exports = {
     cookieSecret: 'BFBWX',
-    db: 'websiteOnline',
+    db: 'website',
     host: '127.0.0.1',
     port: '10105',
-    key: '9d101c97133837e13dde2d32a5054abb', //e6371d360d79eb9fa4c25c7f91d2bc6b
-    mch_id: '7551000001', //101560037142
+    key: '40e4e7dee655f67660723bb78fb7f298', //e6371d360d79eb9fa4c25c7f91d2bc6b
+    mch_id: '101580055204', //101560037142
     create_ip: '127.0.0.1',
-    notify_Url: 'http://www.dushidao.com/admin/pay/notify',
-    AppID: 'wx3fc2b5ecc62fb706', //wxca435d4bf588644b
-    AppSecret: '9aa16bfd1a73d231130e51758036c23c' //c0e00d6f36f35819218032e18fa87a01
+    notify_Url: 'http://www.bfbeducation.com/admin/pay/notify',
+    AppID: 'wxca435d4bf588644b', //wxca435d4bf588644b
+    AppSecret: 'c0e00d6f36f35819218032e18fa87a01' //c0e00d6f36f35819218032e18fa87a01
 };
